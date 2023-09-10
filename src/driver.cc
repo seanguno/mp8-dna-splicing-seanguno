8a1358f6abdbcffac0c5e9894a8fa442658904ca
Sean Guno <91289730+seanguno@users.noreply.github.com>
2023-09-10 13:09:43 -0500

    Update driver.cc

--- src/driver.cc
+++ src/driver.cc
@@ -2,4 +2,26 @@
 
 #include "dna_strand.hpp"
 
-int main() {}
+int main() {
+    /*
+    //DNAstrand test = DNAstrand(*(new Node('c')));
+    DNAstrand test = DNAstrand(*(new Node('g')));
+    //test.PushBack('t');
+    //test.PushBack('g');
+    test.PushBack('a');
+    test.PushBack('a');
+    test.PushBack('t');
+    test.PushBack('t');
+    test.PushBack('c');
+    test.PushBack('a');
+    //test.PushBack('g');
+    DNAstrand to_splice = DNAstrand(*(new Node('t')));
+    to_splice.PushBack('g');
+    to_splice.PushBack('a');
+    to_splice.PushBack('t');
+    to_splice.PushBack('c');
+    test.SpliceIn("gaattc", to_splice);
+    std::cout << test.ToString() << std::endl;
+    */
+
+}
